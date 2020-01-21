ae7f17b62c0d4cf4b7e70e3f754d790348254ede
Cameron Hattendorf <hattendorfcameron@gmail.com>
2020-01-21 09:48:05 -0800

    fixed typos on about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -16,8 +16,8 @@ export default () => (
           it. <br /><br /> Later, in my teens I began hopping freight trains and hitch-hiking around the country -
           mostly enjoying the wide-open expanses of the western U.S.. Busking on street corners and farmers' markets was
           my preferred method of getting myself fed. <br /><br />I am taking a leap and am producing my first full album
-          (name yet to be determined). It's such an exciting endeavor and I have so many folks to thank for my desicion
-          and for thier support so far. <br /><br />If you are curious to find out more, follow this journey and/or
+          (name yet to be determined). It's such an exciting endeavor and I have so many folks to thank for my decision
+          and for their support so far. <br /><br />If you are curious to find out more, follow this journey and/or
           connect and support the process, I urge you to hop on over to my <a href="https://www.patreon.com/Cambomusic"
             aria-label="Link to Patreon Page">Patreon</a> - the home base for this project.</p>
         <h4>Keep On Keepin' On</h4>
