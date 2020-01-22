482a05cd8343b8e154e01b4c6dac654a9c129216
Cameron Hattendorf <hattendorfcameron@gmail.com>
2020-01-22 08:38:56 -0800

    fixed some z-index stuff, edited facebook icon

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -12,4 +12,6 @@ exports.wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
   return <Layout {...props}>{element}</Layout>
+  
+  
 }
